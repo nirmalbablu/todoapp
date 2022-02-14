4345786dbc11d4524896edc0195a0d5a4b7f07ec
nirmalbablu <nirmalbablu20@gmail.com>
2022-02-14 14:54:26 +0530

    modified

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 
 const useLocalStorage = () => {
    const [todo,setTodo] = useState(JSON.parse(localStorage.getItem('todo'))|| []);
